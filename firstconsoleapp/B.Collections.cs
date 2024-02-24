995e9614e4bd12569b9a6c65c5bebd9887e2c730
sona678 <sonamaharjan5@gmail.com>
2024-02-24 07:19:51 +0545

    learn collection

---
+++ firstconsoleapp/B.Collections.cs
@@ -0,0 +1,19 @@
+class Coll
+{
+  void Learn() 
+  {
+    //one dimensional
+    byte[] ages= {12,78,32,76,52};
+    string[] names ={"Sona", "21", "ktm"};
+    //multi dimensional
+    int[,] studentsInClassRoom=new int[7,6];
+    studentsInClassRoom[0,0]=0;
+    studentsInClassRoom[0,0]=1;
+    studentsInClassRoom[0,0]=10;
+
+    //jagged array
+    int[][] studentsInClassRoom2= [[1,2,3],[3,4],[2,4,5,6]];
+
+
+  }
+}
