7f107d336b8757fb56aee673c200b62ec6cf46ac
sona678 <sonamaharjan5@gmail.com>
2024-02-24 14:12:58 +0545

    learn upper case and dob

--- firstconsoleapp/Program.cs
+++ firstconsoleapp/Program.cs
@@ -1,9 +1,25 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-Console.WriteLine("I am main");
-Datatypes dt=new();
-dt.Learn();
-Methods m= new();
-m.Greet("Hi");
+class Name
+{
+    public static void greeter()
+    {
+        Console.WriteLine("Your name:");
+        string fullname = Console.ReadLine();
+        string cfullname = fullname.ToUpper();
+        Console.WriteLine($"Hello, {cfullname} Ji!");
+
+        Console.WriteLine("Your DOB (YYYY/MM/DD):");
+        string dobString = Console.ReadLine();
+        DateTime dob = DateTime.Parse(dobString);
+        string dobFriendly = dob.ToString("MMMM dd, yyyy");
+        Console.WriteLine($"Your date of birth is: {dobFriendly}");
+    }
+    public static void Main(string[] args)
+    {
+        greeter();
+    }
+}
+
+
 
 
