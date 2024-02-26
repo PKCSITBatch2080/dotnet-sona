d4bca36d0bdf0a7a3dacdef08b42fa6bc35b7a6a
sona678 <sonamaharjan5@gmail.com>
2024-02-26 07:01:46 +0545

    calculate age

--- firstconsoleapp/Program.cs
+++ firstconsoleapp/Program.cs
@@ -13,13 +13,26 @@ class Name
         DateTime dob = DateTime.Parse(dobString);
         string dobFriendly = dob.ToString("MMMM dd, yyyy");
         Console.WriteLine($"Your date of birth is: {dobFriendly}");
+        DateTime today = DateTime.Today;
+
+TimeSpan age = today - dob;
+int totalDays = Convert.ToInt32(age.TotalDays);
+int totalHours = Convert.ToInt32(age.TotalHours);
+int totalMinutes = Convert.ToInt32(age.TotalMinutes);
+
+int years = totalDays / 365;
+int months = (totalDays % 365) / 30;
+int weeks = (totalDays % 365 % 30) / 7;
+int days = totalDays % 365 % 30 % 7;
+
+Console.WriteLine($"Hello, {cfullname} Ji");
+Console.WriteLine($"Your DOB is {dob.ToString("dddd, dd MMMM yyyy")}.");
+
+Console.WriteLine($"You are {years} years {months} months {weeks} weeks and {days} days old. \n");
     }
     public static void Main(string[] args)
     {
         greeter();
     }
 }
-
-
-
 
